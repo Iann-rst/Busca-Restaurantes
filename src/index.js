08833e3331f7a7e078f70732b6879503266222ff
Iann-rst <iannrst11@gmail.com>
2021-08-23 18:37:33 -0300

    Início do Desenvolvimento do conteúdo lateral

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@material/react-text-field/dist/text-field.css';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import {Container} from './styles';
+import React, {useState} from 'react';
+import TextField, { Input } from '@material/react-text-field';
+
+
+import logo from '../../assets/logo.svg';
+
+import {Container, Search} from './styles';
+
 
 const Home = () => {
-  return(
-    <Container>Hello world!</Container>
-  );
+
+  const [inputValue, setInputValue] = useState('cachorro');
+    return(
+      <Container>
+        <Search>
+          <img src={logo} alt="Logo do restaurante"/>
+          <TextField
+            label="Pesquisar"
+            outlined
+          // onTrailingIconSelected={()=> this.setState({ value: 'Pesquisar' })}
+          >
+          <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+          />  
+          </TextField>
+        </Search>
+      </Container>
+    );
 }
 export default Home;
