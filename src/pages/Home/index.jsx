754740c3c38f1a8d629194705ddee0d65558e304
Iann-rst <iannrst11@gmail.com>
2021-08-24 12:03:00 -0300

    adicionando icone no TextField

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import TextField, { Input } from '@material/react-text-field';
 
+import SearchIcon from '@material-ui/icons/Search';
 
-import logo from '../../assets/logo.svg';
-
-import {Container, Search} from './styles';
+//import logo from '../../assets/logo.svg';
+import logo2 from '../../assets/logo.png'
+import {Container, Search, Logo} from './styles';
 
 
 const Home = () => {
@@ -13,11 +14,12 @@ const Home = () => {
     return(
       <Container>
         <Search>
-          <img src={logo} alt="Logo do restaurante"/>
+          <Logo src={logo2} alt="Logo do restaurante"/>
           <TextField
-            label="Pesquisar"
+            label="Pesquisar Restaurantes"
             outlined
           // onTrailingIconSelected={()=> this.setState({ value: 'Pesquisar' })}
+            trailingIcon={<SearchIcon role="button" icon="search" />}
           >
           <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)}
           />  
