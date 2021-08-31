bc9d3fea3784b352ab6f79c51e52d8d021e4e20d
Iann-rst <iannrst11@gmail.com>
2021-08-31 14:27:14 -0300

    estilização das informações do restaurante (modal)

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -33,7 +33,7 @@ export const Map = styled.div`
 
 export const Carousel = styled(Slider)`
 .slick-slide{
-  margin-right: 16px;
+  margin-right: 30px;
 }
 `;
 
@@ -46,3 +46,22 @@ export const CarouselTitle = styled.h1`
   margin: 16px 0;
 `;
 
+export const ModalTitle = styled.p`
+  margin-bottom: 10px;
+  letter-spacing: 0.11px;
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  line-height: 29px;
+  font-size: 24px;
+  font-weight: bold;  
+`;
+
+export const ModalContent = styled.p`
+  margin-bottom: 10px;
+  letter-spacing: 0.11px;
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  font-weight: normal;
+  line-height: 19px;
+  font-size: 16px;
+`;
